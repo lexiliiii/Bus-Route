42e43a99974ab932a4117052855ed211d31427da
CaseyyyyS <nqs9yu@virginia.edu>
2024-04-18 23:03:02 -0400

    clean code

--- src/main/java/edu/virginia/sde/hw5/Configuration.java
+++ src/main/java/edu/virginia/sde/hw5/Configuration.java
@@ -57,7 +57,6 @@ public class Configuration {
             busStopsURL = new URL(endpoints.getString("stops"));
             busLinesURL = new URL(endpoints.getString("lines"));
             databaseFilename = json.getString("database");
-            databaseFilename = json.getString("database");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -75,4 +74,5 @@ public class Configuration {
 //            e.printStackTrace();
 //        }
 //    }
+
 }
